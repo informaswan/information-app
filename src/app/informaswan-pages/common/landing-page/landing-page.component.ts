c95cad7ffa6b64ceea8a062e9f6ec14d9cde1c16
SamuelSwanson <77930358+SamuelSwanson@users.noreply.github.com>
2024-09-18 22:17:35 -0400

    Progress on loading images

--- src/app/informaswan-pages/common/landing-page/landing-page.component.ts
+++ src/app/informaswan-pages/common/landing-page/landing-page.component.ts
@@ -18,19 +18,19 @@ export class LandingPageComponent {
   muscle_image = "'assets/images/muscle_front_20.JPG'";
   poker_title = "How to play poker";
   poker_content = "This is a game that will show you how to play poker";
-  poker_image = "'../../../../assets/images/muscle_front_20.JPG'";
+  poker_image = "'assets/images/muscle_front_20.JPG'";
   cocktail_title = "Cocktail search";
   cocktail_content = "This will will populate a list of cocktails you can make with the ingredients you have";
-  cocktail_image = "'../../../../assets/images/muscle_front_20.JPG'";
+  cocktail_image = "'assets/images/muscle_front_20.JPG'";
   home_loan_title = "Home Loan calculators";
   home_loan_content = "This set of calculators can be used to give you an idea of what you can qualify for.";
-  home_loan_image = "'../../../../assets/images/muscle_front_20.JPG'";
+  home_loan_image = "'assets/images/muscle_front_20.JPG'";
   home_care_title = "Retirment Care Cost Comparitors";
   home_care_content = "These comparisons show cost differences between elderly costs.";
-  home_care_image = "'../../../../assets/images/muscle_front_20.JPG'";
+  home_care_image = "'assets/images/muscle_front_20.JPG'";
   disc_golf_instuctional_title = "Disc Golf Form Check";
   disc_golf_instuctional_content = "This website consolidates instructional disc golf topics in one place.";
-  disc_golf_instuctional_image = "'../../../../assets/images/muscle_front_20.JPG'";
+  disc_golf_instuctional_image = "'assets/images/muscle_front_20.JPG'";
   
   doSomething(){
     console.log("doing it")
